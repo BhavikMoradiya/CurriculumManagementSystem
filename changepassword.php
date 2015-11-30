2709105e428e033305fcae1d751df3c77a4a0c90
Nate Carlin <carlin010@knights.gannon.edu>
2015-11-29 19:38:42 -0500

    Manual merge: added changes from Bhavik_Sprint_1 branch

--- changepassword.php
+++ changepassword.php
@@ -1,4 +1,4 @@
-<?php session_start(); 
+<?php session_start();
 if (!(isset($_SESSION["email"])))
 {
 
@@ -6,22 +6,23 @@ if (!(isset($_SESSION["email"])))
 }
 ?>
 
-  <?php include("includes/db_connection.php"); ?> 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<?php include("style.css"); ?>
-</head>
-<?php
+    <?php include("includes/db_connection.php"); ?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+            <?php include("style.css"); ?>
+        </head>
+        <?php
 
 
- 
+
 
 if (isset($_POST['Submit']))
 {
 
-    
+
 $password1=md5($_POST["newpw"]);
 $password1=stripslashes($password1);
 $password1=mysqli_real_escape_string ($mysqli,$password1);
@@ -49,19 +50,22 @@ if (isset($sql)) {
 }
 }
 ?>
-<body>
-<form id="form1" name="form1" method="post" action="">
-  <table class="gradienttable">
-    <tr>
-      <th height="20" colspan="2" scope="row">&nbsp;</th>
-    </tr>
 
-    <tr>
-      <th width="147" height="34" scope="row">New password:</th>
-      <td width="290"><input name="newpw" type="password" id="newpw" required="required" /></td>
-    </tr>
-    <tr>
-      <th width="147" height="31" scope="row"><strong>Confirm Password:</th>
+            <body>
+                <form id="form1" name="form1" method="post" action="">
+                    <table class="gradienttable">
+                        <tr>
+                            <th height="20" colspan="2" scope="row">&nbsp;</th>
+                        </tr>
+
+                        <tr>
+                            <th width="147" height="34" scope="row">New password:</th>
+                            <td width="290">
+                                <input name="newpw" type="password" id="newpw" required="required" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="147" height="31" scope="row"><strong>Confirm Password:</th>
       <td width="290"><input name="retypepw" type="password" id="retypepw" required="required" /></td>
     </tr>
     <tr>
