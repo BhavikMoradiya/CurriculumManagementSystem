17e323a976cc721bd99a74f9261037cd846baf30
James Gruss, Jr <jgrussjr@gmail.com>
2015-12-11 13:19:15 -0500

    fixed sql comment problem, again

--- sql/cms_database_with_data_updated.sql
+++ sql/cms_database_with_data_updated.sql
@@ -337,8 +337,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
 (1, 'tang002@gannon.edu', 'ac86cabae707c1d7b2b9a5749607a3d9'),
-(2, 'abc@gmail.com', 'c20ad4d76fe97759aa27a0c99bff6710') --// password is "12"
-;
+(2, 'abc@gmail.com', 'c20ad4d76fe97759aa27a0c99bff6710');
+-- password for abc@gmail.com is "12"
 
 
 --
